99fbd6355e1dd3e870ca7e5cfcd88461787fce0d
Eugene Helland <eugenehelland@mail.com>
2022-03-17 15:54:41 -0600

    can search for breed and the quality that the user is interested in from thecatapi.com.

---
+++ breedFetcher.js
@@ -0,0 +1,16 @@
+const request = require('request');
+const args = process.argv.slice(2);
+
+const breed = args[0];
+let quality = args[1]
+if (quality === undefined) quality = 'name';
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+  console.log('error:', error);
+  console.log('statusCode:', response && response.statusCode);
+
+  if (JSON.parse(body)[0] === undefined) {
+    return console.log('Sorry, that breed is not present.');
+  }
+  console.log('body:', JSON.parse(body)[0][quality]);
+});
