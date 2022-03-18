7f4452f109f9485ad4613abefab7b34576442a32
Eugene Helland <eugenehelland@mail.com>
2022-03-18 11:02:17 -0600

    fetchBreedDescription now passing written test when attempting to fetch breed that is present in the API database, fetchBreedDescription fails written test when trying to fetch a breed that is not present in the database.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,16 @@
 const request = require('request');
-const args = process.argv.slice(2);
 
-const breed = args[0];
-let quality = args[1];
-if (quality === undefined) quality = 'name';
+const fetchBreedDescription = function(breedName, callback) {
+  const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(URL, (error, response, body) => {
+    let desc = body;
+    if (body.length < 3) {
+      desc = 'That breed is not in our database.';
+    } else {
+      desc = JSON.parse(body)[0].description;
+    }
+    return callback(error, desc);
+  });
+};
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  if (error !== null) return console.log('Broken link.');
-  console.log('statusCode:', response && response.statusCode);
-  if (JSON.parse(body)[0] === undefined) {
-    return console.log('Sorry, that breed is not present.');
-  }
-  console.log('body:', JSON.parse(body)[0][quality]);
-});
+module.exports = { fetchBreedDescription };
